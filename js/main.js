75e6638e6db194748130bf8d03f1d554c4ebcb47
nickrdandrea <nickrdandrea@gmail.com>
2017-02-16 10:31:45 -0500

    Pimped out the switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
+$(document).ready(function() {
+    $("div.switch button").click(function(){
+        $("div.switch button").toggleClass("on");
+        $("div.switch button").toggleClass("off");
 
+        $("body").toggleClass("light");
+        $("body").toggleClass("dark");
 
+        if ($("body").hasClass("light")) {$("h1.status").text("It's so bright in here!");}
+        else {$("h1.status").text("Hey, who turned off the lights?");}
+    });
+});
